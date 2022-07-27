55e6064e60cb85a5b78968767568a228d6873d4f
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2022-07-27 20:35:30 +0430

    Update model.php

--- model.php
+++ model.php
@@ -1,6 +1,6 @@
 <?php
 
-function insert($jsfile){
+function insertScript($jsfile){
 return file_get_contents($jsfile);
 }
 
@@ -15,7 +15,7 @@ console.log = function () {};
 console.warn = function () {};
 console.error = function () {};
  
-".insert("tensorflow.js")." 
+".insertScript("tensorflow.js")." 
 
 const model = tf.sequential();
 model.add(tf.layers.conv2d({filters: 16, kernelSize: [3, 3], activation: 'relu', padding: 'same', inputShape: [28, 28, 1]}));
